449af41a6f6fc3e8407cf830c42e1a500d4dabec
howardzchung@gmail.com <howardzchung@gmail.com>
2020-03-24 23:35:12 -0700

    update turn server

--- src/App.js
+++ src/App.js
@@ -5,16 +5,11 @@ import { v4 as uuidv4 } from 'uuid';
 import querystring from 'querystring';
 
 const iceServers = [
-  { url:'stun:stun.l.google.com:19302'},
+{ url:'stun:stun.l.google.com:19302'},
 {
-	url: 'turn:192.158.29.39:3478?transport=udp',
-	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-	username: '28224511:1379330808'
-},
-{
-	url: 'turn:192.158.29.39:3478?transport=tcp',
-	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-	username: '28224511:1379330808'
+	url: 'turn:numb.viagenie.ca',
+	credential: 'watchparty',
+	username: 'howardzchung@gmail.com'
 }];
 export default class App extends React.Component { 
   state = {
