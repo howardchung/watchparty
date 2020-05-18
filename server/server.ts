90ff3ec71f760da9492d95faf205e7a67d48e7f2
Dominik Biedebach <dominik@bdbch.com>
2020-05-18 23:54:53 +0200

    refactor and fix for typos and utils functions
    Former-commit-id: 4d8a375680107c591a66faf7356c3948c01fdb01

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,16 @@
-require('dotenv').config();
-import fs from 'fs';
-import util from 'util';
-import express from 'express';
-import Moniker from 'moniker';
-import Youtube from 'youtube-api';
-import cors from 'cors';
-import Redis from 'ioredis';
-import https from 'https';
-import http from 'http';
-import socketIO from 'socket.io';
-import { searchYoutube } from './utils/searchYoutube';
+require("dotenv").config();
+import fs from "fs";
+import util from "util";
+import express from "express";
+import Moniker from "moniker";
+import Youtube from "youtube-api";
+import cors from "cors";
+import Redis from "ioredis";
+import https from "https";
+import http from "http";
+import socketIO from "socket.io";
+import { searchYoutube } from "./utils/searchYoutube";
+import { authenticateYoutube } from "./utils/authenticateYoutubeApi";
 
 const app = express();
 let server: any = null;
@@ -20,17 +21,17 @@ if (process.env.HTTPS) {
 } else {
   server = new http.Server(app);
 }
-const io = socketIO(server, { origins: '*:*' });
+const io = socketIO(server, { origins: "*:*" });
 let redis = (undefined as unknown) as Redis.Redis;
 if (process.env.REDIS_URL) {
   redis = new Redis(process.env.REDIS_URL);
 }
-const Room = require('./room');
+const Room = require("./room");
 const {
   resizeVMGroup,
   cleanupVMGroup,
   isVBrowserFeatureEnabled,
-} = require('./vm');
+} = require("./vm");
 
 const names = Moniker.generator([
   Moniker.adjective,
@@ -44,9 +45,9 @@ init();
 async function init() {
   if (process.env.REDIS_URL) {
     // Load rooms from Redis
-    console.log('loading rooms from redis');
-    const keys = await redis.keys('/*');
-    console.log(util.format('found %s rooms in redis', keys.length));
+    console.log("loading rooms from redis");
+    const keys = await redis.keys("/*");
+    console.log(util.format("found %s rooms in redis", keys.length));
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
       const roomData = await redis.get(key);
@@ -66,8 +67,8 @@ async function init() {
     }, 1000);
   }
 
-  if (!rooms.has('/default')) {
-    rooms.set('/default', new Room(io, '/default'));
+  if (!rooms.has("/default")) {
+    rooms.set("/default", new Room(io, "/default"));
   }
 
   if (isVBrowserFeatureEnabled()) {
@@ -81,10 +82,10 @@ async function init() {
         if (room.vBrowser && room.vBrowser.assignTime) {
           if (
             Number(new Date()) - room.vBrowser.assignTime >
-            6 * 60 * 60 * 1000 ||
+              6 * 60 * 60 * 1000 ||
             room.roster.length === 0
           ) {
-            console.log('[RESET] VM in room:', room.roomId);
+            console.log("[RESET] VM in room:", room.roomId);
             room.resetRoomVM();
           }
         }
@@ -95,9 +96,9 @@ async function init() {
       for (let i = 0; i < roomArr.length; i++) {
         const room = roomArr[i];
         if (room.vBrowser && room.vBrowser.id) {
-          console.log('[RENEW] VM in room:', room.roomId, room.vBrowser.id);
+          console.log("[RENEW] VM in room:", room.roomId, room.vBrowser.id);
           // Renew the lock on the VM
-          await redis.expire('vbrowser:' + room.vBrowser.id, 300);
+          await redis.expire("vbrowser:" + room.vBrowser.id, 300);
         }
       }
     };
@@ -110,21 +111,16 @@ async function init() {
   server.listen(process.env.PORT || 8080);
 }
 
-if (process.env.YOUTUBE_API_KEY) {
-  Youtube.authenticate({
-    type: 'key',
-    key: process.env.YOUTUBE_API_KEY,
-  });
-}
+authenticateYoutube();
 
 app.use(cors());
-app.use(express.static('build'));
+app.use(express.static("build"));
 
-app.get('/ping', (req, res) => {
-  res.json('pong');
+app.get("/ping", (req, res) => {
+  res.json("pong");
 });
 
-app.get('/stats', (req, res) => {
+app.get("/stats", (req, res) => {
   if (req.query.key && req.query.key === process.env.STATS_KEY) {
     const roomData: any[] = [];
     rooms.forEach((room) => {
@@ -140,34 +136,35 @@ app.get('/stats', (req, res) => {
       rooms: roomData,
     });
   } else {
-    return res.status(403).json({ error: 'Access Denied' });
+    return res.status(403).json({ error: "Access Denied" });
   }
 });
 
-app.get('/youtube', (req, res) => {
-  if (typeof req.query.q === 'string') {
-    searchYoutube(req.query.q)
-      .then(items => res.json(items))
-      .catch(() => {
-        return res.status(500).json({ error: 'youtube error' });
-      })
+app.get("/youtube", async (req, res) => {
+  if (typeof req.query.q === "string") {
+    try {
+      const items = await searchYoutube(req.query.q);
+      res.json(items);
+    } catch {
+      return res.status(500).json({ error: "youtube error" });
+    }
   } else {
-    return res.status(500).json({ error: 'query must be a string' });
+    return res.status(500).json({ error: "query must be a string" });
   }
 });
 
-app.post('/createRoom', (req, res) => {
+app.post("/createRoom", (req, res) => {
   let name = names.choose();
   // Keep retrying until no collision
   while (rooms.has(name)) {
     name = names.choose();
   }
-  console.log('createRoom: ', name);
-  rooms.set('/' + name, new Room(io, '/' + name));
+  console.log("createRoom: ", name);
+  rooms.set("/" + name, new Room(io, "/" + name));
   res.json({ name });
 });
 
-app.get('/settings', (req, res) => {
+app.get("/settings", (req, res) => {
   if (req.hostname === process.env.CUSTOM_SETTINGS_HOSTNAME) {
     return res.json({
       mediaPath: process.env.MEDIA_PATH,
--- server/utils/searchYoutube.ts
+++ server/utils/searchYoutube.ts
@@ -1,20 +1,21 @@
-import Youtube from 'youtube-api'
-import { mapYoutubeResult, YoutubeVideo } from './mapYoutubeResult';
+import Youtube from "youtube-api";
+import { YoutubeVideo } from "../index.d";
+import { mapYoutubeResult } from "./mapYoutubeResult";
 
 export const searchYoutube = (query: string): Promise<YoutubeVideo[]> => {
   return new Promise((resolve, reject) => {
     Youtube.search.list(
-      { part: 'snippet', type: 'video', maxResults: 25, q: query },
+      { part: "snippet", type: "video", maxResults: 25, q: query },
       (err: any, data: any) => {
-        console.log(err)
+        console.log(err);
         if (data && data.items) {
           const response = data.items.map(mapYoutubeResult);
-          resolve(response)
+          resolve(response);
         } else {
           console.error(data);
-          reject()
+          reject();
         }
       }
     );
-  })
-}
+  });
+};
