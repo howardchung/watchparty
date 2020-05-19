6c0c465b3af03ebd6ba9bf6ea733a06c6882d73e
Dominik Biedebach <dominik@bdbch.com>
2020-05-19 18:10:03 +0200

    add example test again

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
+
 import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders app', () => {
-  render(<App />);
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
