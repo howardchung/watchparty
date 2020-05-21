602af1ac9b265df61615e1fc008b685c638dab37
Dominik Biedebach <dominik@bdbch.com>
2020-05-21 13:49:36 +0200

    added comment for new youtube video id regex

--- server/utils/regex.ts
+++ server/utils/regex.ts
@@ -1,1 +1,2 @@
+/** This regex searches for YouTube Video IDs from a YouTube URL */
 export const YOUTUBE_VIDEO_ID_REGEX = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
