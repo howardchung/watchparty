57e45fbb90ebd6ad583bdca3f5aa8b451240883c
Dominik Biedebach <dominik@bdbch.com>
2020-05-22 13:21:50 +0200

    added example for youtube video regex

--- server/utils/regex.ts
+++ server/utils/regex.ts
@@ -1,4 +1,7 @@
 /** This regex searches for YouTube Video IDs from a YouTube URL */
+/** example: */
+/** YOUTUBE_VIDEO_ID_REGEX.exec('https://youtube.com/?v=14634524364) */
+/** will return the id 14634524364 in the first exec group [1] */
 export const YOUTUBE_VIDEO_ID_REGEX = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
 
 /** These regexes allow us to find hours, minutes and seconds from a ISO 8601 time string */
