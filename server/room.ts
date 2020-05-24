1760935d7c41af6d58cc9d59c0cbbe56a980e4ef
Ubuntu <howardzchung@gmail.com>
2020-05-24 23:48:53 +0000

    add dirty state to rooms that are modified by background process
    Former-commit-id: 5e95445e39d7c4505a0166bf8faef8a50d5187d2

--- server/room.ts
+++ server/room.ts
@@ -19,6 +19,7 @@ export class Room {
   public roomId: string;
   public creationTime: Date = new Date();
   private vmManager: VMManager;
+  public isRoomDirty = false; // Indicates the room needs to be saved, e.g. we unassign a VM from an empty room
 
   constructor(
     io: SocketIO.Server,
@@ -295,6 +296,7 @@ export class Room {
       this.roster[i].isController = false;
     });
     this.cmdHost(undefined, '');
+    this.isRoomDirty = true;
     if (id) {
       try {
         await this.vmManager.resetVM(id);
