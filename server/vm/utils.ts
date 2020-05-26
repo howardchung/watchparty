54863f7a68eae0a4f540cd96f10390addc2dcec7
Ubuntu <howardzchung@gmail.com>
2020-05-26 09:33:27 +0000

    update resolution passing

--- server/vm/utils.ts
+++ server/vm/utils.ts
@@ -1,5 +1,6 @@
 export const cloudInit = (
   imageName: string,
+  resolution = '1280x720@30',
   hetznerCloudInit = false
 ) => `#!/bin/bash
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
@@ -10,12 +11,13 @@ ${
     : ''
 }
 PASSWORD=$(hostname)
-docker run -d --rm --name=vbrowser -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e SCREEN="1280x720@30" -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" ${imageName}
+docker run -d --rm --name=vbrowser -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" ${imageName}
 `;
 
 export const cloudInitWithTls = (
   host: string,
-  imageName: string
+  imageName: string,
+  resolution = '1280x720@30'
 ) => `#!/bin/bash
 until nslookup ${host}
 do
@@ -30,7 +32,7 @@ chmod -R 755 /etc/letsencrypt/archive
 # start browser
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 5000
 sed -i 's/scripts-user$/\[scripts-user, always\]/' /etc/cloud/cloud.cfg
-docker run -d --rm --name=vbrowser -v /etc/letsencrypt/archive/${host}:/cert -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e SCREEN="1280x720@30" -e NEKO_PASSWORD=$(hostname) -e NEKO_PASSWORD_ADMIN=$(hostname) -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_KEY="/cert/privkey1.pem" -e NEKO_CERT="/cert/fullchain1.pem" ${imageName}
+docker run -d --rm --name=vbrowser -v /etc/letsencrypt/archive/${host}:/cert -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$(hostname) -e NEKO_PASSWORD_ADMIN=$(hostname) -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_KEY="/cert/privkey1.pem" -e NEKO_CERT="/cert/fullchain1.pem" ${imageName}
 `;
 
 export const imageName = 'nurdism/neko:chromium';
