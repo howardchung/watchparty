45acee46cce117ffa2731bf1c624f7e33259acfe
Ubuntu <howardzchung@gmail.com>
2020-05-26 07:39:01 +0000

    remove unused import
    Former-commit-id: 97e11eff5d8f55ba7a65e66951e49b8a22f861d7

--- src/components/VideoChat/VideoChat.tsx
+++ src/components/VideoChat/VideoChat.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Icon, Popup } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 import { Socket } from 'socket.io';
 
 import {
