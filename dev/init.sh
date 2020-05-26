62b16bbbe61effcc99a52db7961b23897c0c951a
Ubuntu <howardzchung@gmail.com>
2020-05-26 11:31:27 +0000

    remove old references
    Former-commit-id: 55990ad3d7ab23e96ad49f922cb6132cee119357

--- dev/init.sh
+++ dev/init.sh
@@ -12,8 +12,3 @@ docker run -d --network=host instrumentisto/coturn \
   --realm=watchparty \  
 # TODO use turnadmin to create username and password
 
-# test vbrowser with ssl
-docker run -d --rm --name=vbrowser -v /etc/letsencrypt/archive/azure.howardchung.net:/cert -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e SCREEN="1280x720@30" -e NEKO_PASSWORD=neko -e NEKO_PASSWORD_ADMIN=admin -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_KEY="/cert/privkey1.pem" -e NEKO_CERT="/cert/fullchain1.pem" nurdism/neko:chromium
-
-# without ssl
-docker run -d --rm --name=vbrowser -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e SCREEN="1280x720@30" -e NEKO_PASSWORD=neko -e NEKO_PASSWORD_ADMIN=admin -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" nurdism/neko:chromium
