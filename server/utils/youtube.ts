d95238134f7a1b923c2f9df102d189f06739ae07
Ubuntu <howardzchung@gmail.com>
2020-05-28 13:27:29 +0000

    partial changes for playlist feature: youtube and utils

--- server/utils/youtube.ts
+++ server/utils/youtube.ts
@@ -1,5 +1,16 @@
 import Youtube from 'youtube-api';
-import { YoutubeVideo, YoutubeSearchResult } from '../index.d';
+import {
+  PlaylistVideo,
+  YoutubeAPIVideoResult,
+  YoutubeListResult,
+  YoutubeSearchResult,
+} from '../index.d';
+import {
+  PT_HOURS_REGEX,
+  PT_MINUTES_REGEX,
+  PT_SECONDS_REGEX,
+  YOUTUBE_VIDEO_ID_REGEX,
+} from './regex';
 
 if (process.env.YOUTUBE_API_KEY) {
   Youtube.authenticate({
@@ -8,7 +19,9 @@ if (process.env.YOUTUBE_API_KEY) {
   });
 }
 
-export const mapYoutubeResult = (video: YoutubeSearchResult): YoutubeVideo => {
+export const mapYoutubeSearchResult = (
+  video: YoutubeSearchResult
+): PlaylistVideo => {
   return {
     channel: video.snippet.channelTitle,
     url: 'https://www.youtube.com/watch?v=' + video.id.videoId,
@@ -17,14 +30,27 @@ export const mapYoutubeResult = (video: YoutubeSearchResult): YoutubeVideo => {
   };
 };
 
-export const searchYoutube = (query: string): Promise<YoutubeVideo[]> => {
+export const mapYoutubeListResult = (
+  video: YoutubeListResult
+): PlaylistVideo => {
+  const videoId = video.id;
+  return {
+    url: 'https://www.youtube.com/watch?v=' + videoId,
+    name: video.snippet.title,
+    img: video.snippet.thumbnails.default.url,
+    channel: video.snippet.channelTitle,
+    duration: getVideoDuration(video.contentDetails.duration),
+  };
+};
+
+export const searchYoutube = (query: string): Promise<PlaylistVideo[]> => {
   return new Promise((resolve, reject) => {
     Youtube.search.list(
       { part: 'snippet', type: 'video', maxResults: 25, q: query },
       (err: any, data: any) => {
         console.log(err);
         if (data && data.items) {
-          const response = data.items.map(mapYoutubeResult);
+          const response = data.items.map(mapYoutubeSearchResult);
           resolve(response);
         } else {
           console.error(data);
@@ -33,4 +59,49 @@ export const searchYoutube = (query: string): Promise<YoutubeVideo[]> => {
       }
     );
   });
+};
+
+export const getYoutubeVideoID = (url: string) => {
+  const idParts = YOUTUBE_VIDEO_ID_REGEX.exec(url);
+  if (!idParts) {
+    return;
+  }
+
+  const id = idParts[1];
+  if (!id) {
+    return;
+  }
+
+  return id;
+};
+
+export const fetchYoutubeVideo = (id: string): Promise<PlaylistVideo> => {
+  return new Promise((resolve, reject) => {
+    Youtube.videos.list(
+      { part: 'snippet,contentDetails', id },
+      (err: any, data: YoutubeAPIVideoResult) => {
+        if (data) {
+          const video = data.items[0];
+          resolve(mapYoutubeListResult(video));
+        } else {
+          console.error(err);
+          console.log(err);
+          reject('unknown youtube api error');
+        }
+      }
+    );
+  });
+};
+
+export const getVideoDuration = (string: string) => {
+  const hoursParts = PT_HOURS_REGEX.exec(string);
+  const minutesParts = PT_MINUTES_REGEX.exec(string);
+  const secondsParts = PT_SECONDS_REGEX.exec(string);
+
+  const hours = hoursParts ? parseInt(hoursParts[1]) : 0;
+  const minutes = minutesParts ? parseInt(minutesParts[1]) : 0;
+  const seconds = secondsParts ? parseInt(secondsParts[1]) : 0;
+
+  const totalSeconds = seconds + minutes * 60 + hours * 60 * 60;
+  return totalSeconds;
 };
