98c5ee0fb5cbd187803d9d43fe40bc727b6c6ae6
Ubuntu <howardzchung@gmail.com>
2020-05-31 11:12:02 +0000

    add utils for firebase and stripe
    Former-commit-id: 0a6e6337fd852c7242e529285cf6fb5da10a74db

---
+++ server/utils/firebase.ts
@@ -0,0 +1,20 @@
+import * as admin from 'firebase-admin';
+
+if (process.env.FIREBASE_ADMIN_SDK_CONFIG) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG)
+    ),
+  });
+}
+
+export async function validateUserToken(uid: string, token: string) {
+  if (!process.env.FIREBASE_ADMIN_SDK_CONFIG) {
+    return undefined;
+  }
+  const decoded = await admin.auth().verifyIdToken(token);
+  if (uid !== decoded.uid) {
+    return undefined;
+  }
+  return decoded;
+}
