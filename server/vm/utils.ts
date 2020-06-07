2f1c75887a3d60d24cb8fd888c9f01e319ec1b91
Ubuntu <howardzchung@gmail.com>
2020-06-07 18:09:19 +0000

    pull latest docker images on boot

--- server/vm/utils.ts
+++ server/vm/utils.ts
@@ -12,6 +12,7 @@ ${
     : ''
 }
 PASSWORD=$(hostname)
+docker pull ${imageName}
 docker run -d --rm --name=vbrowser -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_VP9="${
   vp9 ? 1 : 0
 }" ${imageName}
@@ -36,9 +37,10 @@ chmod -R 755 /etc/letsencrypt/archive
 # start browser
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 5000
 sed -i 's/scripts-user$/\[scripts-user, always\]/' /etc/cloud/cloud.cfg
+docker pull ${imageName}
 docker run -d --rm --name=vbrowser -v /etc/letsencrypt/archive/${host}:/cert -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$(hostname) -e NEKO_PASSWORD_ADMIN=$(hostname) -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_KEY="/cert/privkey1.pem" -e NEKO_CERT="/cert/fullchain1.pem" -e NEKO_VP9="${
   vp9 ? 1 : 0
 }" ${imageName}
 `;
 
-export const imageName = 'howardc93/vbrowser';
+export const imageName = 'howardc93/vbrowser:latest';
