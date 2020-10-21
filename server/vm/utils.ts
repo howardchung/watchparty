92a5a81bbbd13b95fdf4af8866c122d61326f909
Ubuntu <howardzchung@gmail.com>
2020-10-21 08:43:54 +0000

    update image
    Former-commit-id: 01b0c1b0ba3001b0f33145b2b06b8391537d9c8d

--- server/vm/utils.ts
+++ server/vm/utils.ts
@@ -12,7 +12,7 @@ ${
     : ''
 }
 PASSWORD=$(hostname)
-docker pull ${imageName}
+# docker pull ${imageName}
 docker run -d --rm --name=vbrowser -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_VP9="${
   vp9 ? 1 : 0
 }" ${imageName}
@@ -37,7 +37,7 @@ chmod -R 755 /etc/letsencrypt/archive
 # start browser
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 5000
 sed -i 's/scripts-user$/\[scripts-user, always\]/' /etc/cloud/cloud.cfg
-docker pull ${imageName}
+# docker pull ${imageName}
 docker run -d --rm --name=vbrowser -v /etc/letsencrypt:/etc/letsencrypt -v /usr/share/fonts:/usr/share/fonts --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$(hostname) -e NEKO_PASSWORD_ADMIN=$(hostname) -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_KEY="/etc/letsencrypt/live/${host}/privkey.pem" -e NEKO_CERT="/etc/letsencrypt/live/${host}/fullchain.pem" -e NEKO_VP9="${
   vp9 ? 1 : 0
 }" ${imageName}
