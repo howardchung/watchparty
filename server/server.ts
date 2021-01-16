105ee8574f7c5e17fd43d1f2cac00ea4c9ba70e0
Ubuntu <howardzchung@gmail.com>
2021-01-16 01:48:07 +0000

    add syncSubscribers

--- server/server.ts
+++ server/server.ts
@@ -19,8 +19,13 @@ import { Hetzner } from './vm/hetzner';
 import { DigitalOcean } from './vm/digitalocean';
 import { Docker } from './vm/docker';
 import { VMManager } from './vm/base';
-import { getCustomerByEmail, createSelfServicePortal } from './utils/stripe';
-import { validateUserToken } from './utils/firebase';
+import {
+  getCustomerByEmail,
+  createSelfServicePortal,
+  getAllCustomers,
+  getAllSubscriptions,
+} from './utils/stripe';
+import { getUserByEmail, validateUserToken } from './utils/firebase';
 import path from 'path';
 import { Client } from 'pg';
 
@@ -93,7 +98,69 @@ vmManagerLarge?.runBackgroundJobs();
 vmManager?.runReleaseRenew(rooms);
 vmManagerLarge?.runReleaseRenew(rooms);
 const vmManagers = { standard: vmManager, large: vmManagerLarge };
+syncSubscribers();
 init();
+
+async function syncSubscribers() {
+  if (!config.STRIPE_SECRET_KEY || !config.FIREBASE_ADMIN_SDK_CONFIG) {
+    return;
+  }
+  console.time('syncSubscribers');
+  // Fetch subs, customers from stripe
+  const [subs, customers] = await Promise.all([
+    getAllSubscriptions(),
+    getAllCustomers(),
+  ]);
+
+  const emailMap = new Map();
+  customers.forEach((cust) => {
+    emailMap.set(cust.id, cust.email);
+  });
+
+  const uidMap = new Map();
+  for (let i = 0; i < subs.length; i += 10) {
+    // Batch customers and fetch firebase data
+    const batch = subs.slice(i, i + 100);
+    const fbUsers = await Promise.all(
+      batch
+        .map((sub) =>
+          emailMap.get(sub.customer)
+            ? getUserByEmail(emailMap.get(sub.customer))
+            : null
+        )
+        .filter(Boolean)
+    );
+    fbUsers.forEach((user) => {
+      uidMap.set(user?.email, user?.uid);
+    });
+  }
+
+  // Create sub objects
+  const result = subs.map((sub) => ({
+    customerId: sub.customer,
+    email: emailMap.get(sub.customer),
+    status: sub.status,
+    uid: uidMap.get(emailMap.get(sub.customer)),
+  }));
+
+  // Upsert to DB
+  // console.log(result);
+  await postgres?.query('BEGIN TRANSACTION');
+  await postgres?.query('DELETE FROM subscriber');
+  for (let i = 0; i < result.length; i++) {
+    const row = result[i];
+    const columns = Object.keys(row);
+    const values = Object.values(row);
+    const query = `INSERT INTO subscriber(${columns
+      .map((c) => `"${c}"`)
+      .join(',')})
+    VALUES (${values.map((_, i) => '$' + (i + 1)).join(',')})
+    RETURNING *`;
+    await postgres?.query(query, values);
+  }
+  await postgres?.query('COMMIT');
+  console.timeEnd('syncSubscribers');
+}
 
 async function saveRooms() {
   while (true) {
