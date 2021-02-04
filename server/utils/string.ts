04e9f936254dca5736478c6e9e4bcb83a8c2e111
Ubuntu <howardzchung@gmail.com>
2021-02-04 21:02:28 +0000

    use hashing to determine release batches
    Former-commit-id: 29d813bbda217cb789c6376610b9da8269f70382

---
+++ server/utils/string.ts
@@ -0,0 +1,11 @@
+export function hashString(input: string) {
+  var hash = 0,
+    i,
+    chr;
+  for (i = 0; i < input.length; i++) {
+    chr = input.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
