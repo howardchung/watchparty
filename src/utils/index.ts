e9c23f4cedc4d886d3b837256be5c897f8b938ef
Ubuntu <howardzchung@gmail.com>
2021-02-05 06:50:05 +0000

    use a simpler hash function with better distribution

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -34,13 +34,10 @@ export function formatSpeed(input: number) {
 }
 
 export function hashString(input: string) {
-  var hash = 0,
-    i,
-    chr;
-  for (i = 0; i < input.length; i++) {
-    chr = input.charCodeAt(i);
-    hash = (hash << 5) - hash + chr;
-    hash |= 0; // Convert to 32bit integer
+  var hash = 0;
+  for (var i = 0; i < input.length; i++) {
+    var charCode = input.charCodeAt(i);
+    hash += charCode;
   }
   return hash;
 }
