6939748975e107d5dd6b58e705df157a0af6cec0
Ubuntu <howardzchung@gmail.com>
2021-02-05 04:34:26 +0000

    update turn servers
    Former-commit-id: ba611858dac223e4b19a39c858f23a1bdc09794e

--- dev/coturn.sh
+++ dev/coturn.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# install docker
+curl -fsSL https://get.docker.com | sh
+
 # coturn
-docker run -d --network=host instrumentisto/coturn \
+docker run -d --network=host --name=coturn instrumentisto/coturn \
   -n --log-file=stdout \
   --lt-cred-mech --fingerprint \
   --no-multicast-peers --no-cli \
   --no-tlsv1 --no-tlsv1_1 \
-  --realm=watchparty \  
+  --realm=watchparty \
+docker exec -it coturn 'sh -c turnadmin --add -u username -p password -r watchparty'
 # TODO use turnadmin to create username and password
 
