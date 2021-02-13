84a2e388e77166628f0d0a0810a3112f071c9285
Ubuntu <howardzchung@gmail.com>
2021-02-13 15:04:40 +0000

    slow down checking

--- server/vm/base.ts
+++ server/vm/base.ts
@@ -264,7 +264,7 @@ export abstract class VMManager {
     };
 
     const checkStaging = async () => {
-      const checkStagingInterval = 500;
+      const checkStagingInterval = 1000;
       while (true) {
         try {
           // Loop through staging list and check if VM is ready
@@ -323,25 +323,7 @@ export abstract class VMManager {
                 await this.redis.lpush('vBrowserStageRetries', retryCount);
                 await this.redis.ltrim('vBrowserStageRetries', 0, 49);
               } else {
-                if (retryCount % 25 === 0) {
-                  const vm = await this.getVM(id);
-                  console.log(
-                    '[CHECKSTAGING] attempt to poweron and attach to network'
-                  );
-                  this.powerOn(id);
-                  if (!vm?.private_ip) {
-                    this.attachToNetwork(id);
-                  }
-                }
-                if (retryCount % 5 === 0) {
-                  console.log(
-                    '[CHECKSTAGING] not ready:',
-                    id,
-                    host,
-                    retryCount
-                  );
-                }
-                if (retryCount > 100) {
+                if (retryCount >= 100) {
                   console.log('[CHECKSTAGING] giving up:', id);
                   await this.redis
                     .multi()
@@ -350,6 +332,25 @@ export abstract class VMManager {
                     .exec();
                   redisCount('vBrowserStagingFails');
                   await this.resetVM(id);
+                } else {
+                  if (retryCount % 25 === 0) {
+                    const vm = await this.getVM(id);
+                    console.log(
+                      '[CHECKSTAGING] attempt to poweron and attach to network'
+                    );
+                    this.powerOn(id);
+                    if (!vm?.private_ip) {
+                      this.attachToNetwork(id);
+                    }
+                  }
+                  if (retryCount % 5 === 0) {
+                    console.log(
+                      '[CHECKSTAGING] not ready:',
+                      id,
+                      host,
+                      retryCount
+                    );
+                  }
                 }
               }
               resolve();
