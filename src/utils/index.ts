63082e84ee1147853b4c925e1afe6de9073756bf
Ubuntu <howardzchung@gmail.com>
2021-05-22 18:11:01 +0000

    switch to median for getleadertime
    Former-commit-id: e21a9719d06d15a83afcaa453aa44a6d22a3460b

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -296,3 +296,25 @@ export function getAndSaveClientId() {
   }
   return clientId;
 }
+
+export function calculateMedian(array: Array<number>): number {
+  // Check If Data Exists
+  if (array.length >= 1) {
+    // Sort Array
+    array = array.sort((a: number, b: number) => {
+      return a - b;
+    });
+
+    // Array Length: Even
+    if (array.length % 2 === 0) {
+      // Average Of Two Middle Numbers
+      return (array[array.length / 2 - 1] + array[array.length / 2]) / 2;
+    }
+    // Array Length: Odd
+    else {
+      // Middle Number
+      return array[(array.length - 1) / 2];
+    }
+  }
+  return 0;
+}
