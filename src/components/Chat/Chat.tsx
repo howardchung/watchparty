c63c980ecff3c13b0ee963b24873f7d9a85f3218
Ubuntu <howardzchung@gmail.com>
2021-05-31 18:05:38 +0000

    play next on empty
    Former-commit-id: 9b2a6250cb3de6c14584a1dbb9418ec6354c5df2

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -97,7 +97,14 @@ export class Chat extends React.Component<ChatProps> {
         </React.Fragment>
       );
     } else if (cmd === 'playlistAdd') {
-      return `added to the playlist: ${msg}`;
+      return (
+        <React.Fragment>
+          {`added to the playlist: `}
+          <span style={{ textTransform: 'initial' }}>
+            {this.props.getMediaDisplayName(msg)}
+          </span>
+        </React.Fragment>
+      );
     } else if (cmd === 'seek') {
       return `jumped to ${formatTimestamp(msg)}`;
     } else if (cmd === 'play') {
