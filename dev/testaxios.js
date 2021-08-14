886ab64b3937687d3af7de501344166766d06665
Ubuntu <howardzchung@gmail.com>
2021-08-14 17:20:11 +0000

    back to get

---
+++ dev/testaxios.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+start();
+
+async function start() {
+  try {
+    const resp = await axios({
+      method: 'GET',
+      url: 'https://gateway3.watchparty.me/?ip=10.1.0.81/healthz',
+      //timeout: 1000
+    });
+    console.log(resp);
+  } catch (ex) {
+    console.log(ex);
+  }
+}
