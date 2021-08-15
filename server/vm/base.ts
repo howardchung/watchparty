a3a6e0106cea9e9d01328fbafabb3b5d89976dfd
Ubuntu <howardzchung@gmail.com>
2021-08-15 05:21:23 +0000

    add verbose

--- server/vm/base.ts
+++ server/vm/base.ts
@@ -383,7 +383,7 @@ export abstract class VMManager {
     const checkVMReady = async (host: string) => {
       const url = 'https://' + host + '/healthz';
       try {
-        execSync(`curl --ipv4 '${host}'`);
+        execSync(`curl -v --ipv4 '${host}'`);
         // await axios({
         //   method: 'GET',
         //   url,
