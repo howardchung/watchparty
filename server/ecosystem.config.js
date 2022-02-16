f6fcc1992bc84727501b7065472de247ebca88c7
Ubuntu <howardzchung@gmail.com>
2022-02-16 11:15:11 +0000

    update hetzner image again
    Former-commit-id: a1c55ced3d2eeb032dbe6331a4c2ae38751f9198

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -45,7 +45,7 @@ module.exports = {
         HETZNER_GATEWAY_US: 'gateway2.watchparty.me',
         HETZNER_NETWORKS_US: '1258313,1258314,1258315',
         HETZNER_SSH_KEYS: '1570536',
-        HETZNER_IMAGE: '61622581',
+        HETZNER_IMAGE: '61626502',
         SCW_GATEWAY: 'gateway1.watchparty.me',
         SCW_IMAGE: '',
         DO_GATEWAY: 'gateway4.watchparty.me',
