e6314309a37615cda3d65d8871e0cf56e45c9278
Ubuntu <howardzchung@gmail.com>
2022-02-16 13:00:36 +0000

    update image

--- server/vm/utils.ts
+++ server/vm/utils.ts
@@ -9,22 +9,9 @@ import { Docker } from './docker';
 export const cloudInit = (
   imageName: string,
   resolution = '1280x720@30',
-  vp9 = false,
-  hetznerCloudInit = false,
-  h264 = false
 ) => `#!/bin/bash
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
-sed -i 's/scripts-user$/\[scripts-user, always\]/' /etc/cloud/cloud.cfg
-${
-  hetznerCloudInit
-    ? `sed -i 's/scripts-user$/\[scripts-user, always\]/' /etc/cloud/cloud.cfg.d/90-hetznercloud.cfg`
-    : ''
-}
 PASSWORD=$(hostname)
-# docker pull ${imageName}
-docker run -d --rm --name=vbrowser --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_VP9="${
-  vp9 ? 1 : 0
-}" -e NEKO_H264="${h264 ? 1 : 0}" ${imageName}
+docker run -d --rm --name=vbrowser --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_H264="1" ${imageName}
 `;
 
 export const imageName = 'howardc93/vbrowser';
