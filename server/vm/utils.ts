867580b5d3eb1c106fe50ca0eead581baea14e48
Ubuntu <howardzchung@gmail.com>
2022-02-20 02:13:46 +0000

    update image with bitrate
    Former-commit-id: c8b3bd835774d898eea4ae2e477baadc89909d3d

--- server/vm/utils.ts
+++ server/vm/utils.ts
@@ -11,7 +11,7 @@ export const cloudInit = (
   resolution = '1280x720@30'
 ) => `#!/bin/bash
 PASSWORD=$(hostname)
-docker run -d --rm --name=vbrowser --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_H264="1" ${imageName}
+docker run -d --rm --name=vbrowser --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN="${resolution}" -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_H264="1" -e NEKO_VIDEO_BITRATE=10000 ${imageName}
 `;
 
 export const imageName = 'howardc93/vbrowser';
