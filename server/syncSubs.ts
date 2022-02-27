42ddc9bc8242328eb7e2e2c093565bfbf4be5bd6
Argn0 <Argn0@users.noreply.github.com>
2022-02-27 03:26:47 +0100

    review 2

--- server/syncSubs.ts
+++ server/syncSubs.ts
@@ -1,7 +1,5 @@
-import { Intents } from 'discord.js';
 import { Client } from 'pg';
 import config from './config';
-import { DiscordBot } from './utils/discord';
 import { getUserByEmail } from './utils/firebase';
 import { insertObject, updateObject } from './utils/postgres';
 import { getAllActiveSubscriptions, getAllCustomers } from './utils/stripe';
@@ -88,59 +86,6 @@ async function syncSubscribers() {
       process.exit(1);
     }
   }
-
-  const client = new DiscordBot({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
-  });
-  try {
-    await client.login(config.DISCORD_BOT_TOKEN);
-    const guild = client.guilds.cache.get(config.DISCORD_SERVER_ID);
-    const members = await guild?.members.fetch();
-
-    // from discord API get username and discriminator of all users on discord server without a sub role
-    const membersWithoutRole = members
-      ?.filter((member) =>
-        member.roles.cache.some(
-          (role) => role.id !== config.DISCORD_SUB_ROLE_ID
-        )
-      )
-      .map((member) => ({
-        username: member.user.username,
-        discriminator: member.user.discriminator,
-      }));
-    // from DB get username, discriminator and email of everyone who linked their discord
-    const discordResult = await postgres2?.query('SELECT * FROM discord');
-
-    // go through all users on server without a sub role,
-    // if they are a sub, assign them sub role
-    for (const member of membersWithoutRole ?? []) {
-      // check if they linked their discord
-      // (they might have linked their discord account with multiple emails)
-      const rows = discordResult.rows.filter(
-        (row) =>
-          row.username === member.username &&
-          row.discriminator === member.discriminator
-      );
-      // if sub exists for email assign role
-      for (const row of rows) {
-        if (result.some((sub) => sub.email === row.email)) {
-          await client.assignRole(row.username, row.discriminator);
-        }
-      }
-    }
-    // get all rows with null email that are supposed to be cleaned up
-    const nullResult = await postgres2?.query(
-      'SELECT * FROM discord WHERE email IS NULL'
-    );
-    for (const row of nullResult.rows) {
-      await client.assignRole(row.username, row.discriminator, true);
-    }
-    await postgres2?.query('DELETE FROM discord WHERE email IS NULL');
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-
   lastSubs = currentSubs;
   console.log('%s subscribers', result.length);
   console.timeEnd('syncSubscribers');
