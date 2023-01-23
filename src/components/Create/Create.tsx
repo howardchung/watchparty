ca668a1e61cfd455a85b427abc00eba6b4f49b94
Ubuntu <howardzchung@gmail.com>
2023-01-23 10:20:40 +0000

    add a spinner
    Former-commit-id: c2f7e18d88884feabb2ffe4c3f98abbe3cf0fe1a

--- src/components/Create/Create.tsx
+++ src/components/Create/Create.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import firebase from 'firebase/compat/app';
 import { createRoom } from '../TopBar/TopBar';
 import querystring from 'querystring';
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 export const Create = ({ user }: { user: firebase.User | undefined }) => {
   const buttonEl = useRef<HTMLButtonElement>(null);
@@ -9,17 +10,21 @@ export const Create = ({ user }: { user: firebase.User | undefined }) => {
     buttonEl?.current?.click();
   }, 1000);
   return (
-    <button
-      style={{ display: 'none' }}
-      ref={buttonEl}
-      onClick={() => {
-        console.log(user);
-        createRoom(
-          user,
-          false,
-          querystring.parse(window.location.search.substring(1)).video as string
-        );
-      }}
-    />
+    <Dimmer active>
+      <Loader>Creating room. . .</Loader>
+      <button
+        style={{ display: 'none' }}
+        ref={buttonEl}
+        onClick={() => {
+          console.log(user);
+          createRoom(
+            user,
+            false,
+            querystring.parse(window.location.search.substring(1))
+              .video as string
+          );
+        }}
+      />
+    </Dimmer>
   );
 };
