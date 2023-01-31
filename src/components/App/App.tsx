cfa4dbec876adea9cafd754a13d845fc7ef1be02
Ubuntu <howardzchung@gmail.com>
2023-01-31 21:24:19 +0000

    await serviceworker

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1033,11 +1033,11 @@ export default class App extends React.Component<AppProps, AppState> {
           hls.attachMedia(leftVideo);
         } else if (src.startsWith('magnet:')) {
           // WebTorrent
-          // magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel
+          // magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent
           client?._server?.close();
           client?.destroy();
           client = new window.WebTorrent();
-          navigator.serviceWorker?.register('sw.min.js');
+          await navigator.serviceWorker?.register('sw.min.js');
           const controller = await navigator.serviceWorker.ready;
           await new Promise((resolve) => setTimeout(resolve, 500));
           console.log(controller, controller.active?.state);
