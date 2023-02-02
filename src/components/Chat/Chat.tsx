62128621e44949fe21a681d413963bb0a5cba1c4
Ubuntu <howardzchung@gmail.com>
2023-02-02 01:55:47 +0000

    add networking for playback rate

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -171,6 +171,8 @@ export class Chat extends React.Component<ChatProps> {
       return `started the video at ${formatTimestamp(msg)}`;
     } else if (cmd === 'pause') {
       return `paused the video at ${formatTimestamp(msg)}`;
+    } else if (cmd === 'playbackRate') {
+      return `set the playback rate to ${msg}x`;
     } else if (cmd === 'lock') {
       return `locked the room`;
     } else if (cmd === 'unlock') {
