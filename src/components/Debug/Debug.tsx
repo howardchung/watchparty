ce7f7827576e9a2ac6675f43b8d8fd443f26b971
Ubuntu <howardzchung@gmail.com>
2023-02-16 20:23:31 +0000

    fix the debug charts
    Former-commit-id: cf19927ea9731e92996f1f85e1ac0a0a33d0c952

--- src/components/Debug/Debug.tsx
+++ src/components/Debug/Debug.tsx
@@ -11,6 +11,7 @@ import {
 import { serverPath } from '../../utils';
 
 const timeSeriesUrl = serverPath + `/timeSeries${window.location.search}`;
+// const timeSeriesUrl = 'https://www.watchparty.me' + `/timeSeries${window.location.search}`;
 
 const Debug = () => {
   const [data, setData] = useState([]);
@@ -24,28 +25,33 @@ const Debug = () => {
     }) as any,
     [setData]
   );
+  // Get the keys from the last/mostrecent element
   const keys = Object.keys(data.slice(-1)[0] ?? {});
   return (
     <>
-      {keys.map((key) => (
-        <LineChart
-          width={1400}
-          height={400}
-          data={data}
-          margin={{
-            top: 5,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid />
-          <XAxis dataKey="time" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey={key} stroke="#8884d8" />
-        </LineChart>
-      ))}
+      {keys
+        .filter((k) => k !== 'time')
+        .map((key) => {
+          return (
+            <LineChart
+              width={1400}
+              height={400}
+              data={data}
+              margin={{
+                top: 5,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis dataKey="time" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey={key} stroke="#8884d8" />
+            </LineChart>
+          );
+        })}
     </>
   );
 };
