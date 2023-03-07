3284f981a0f86690f21f924e0a8a272b37a97de6
Ubuntu <howardzchung@gmail.com>
2023-03-07 06:19:40 +0000

    don't jump to leader for hls

--- src/components/App/HTML.ts
+++ src/components/App/HTML.ts
@@ -42,9 +42,11 @@ export class HTML implements Player {
     const leftVideo = this.getVideoEl();
     if (leftVideo) {
       leftVideo.srcObject = null;
-      leftVideo.currentTime = time;
+
+      // Clear subtitles
       this.setSubtitleMode('hidden');
       leftVideo.innerHTML = '';
+
       // Check for HLS
       // https://moctobpltc-i.akamaihd.net/hls/live/571329/eight/playlist.m3u8
       if (
@@ -54,7 +56,11 @@ export class HTML implements Player {
         let hls = new window.Hls();
         hls.loadSource(src);
         hls.attachMedia(leftVideo);
-      } else if (src.startsWith('magnet:')) {
+        return;
+      }
+      // Set to room time (don't do for HLS streams since durations will be different)
+      leftVideo.currentTime = time;
+      if (src.startsWith('magnet:')) {
         // WebTorrent
         // magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent
         window.watchparty.webtorrent?._server?.close();
