6e34d13f073e818241433fba5c5e7775df60e2af
Ubuntu <howardzchung@gmail.com>
2023-05-13 17:12:06 +0000

    update vbrowser image

--- server/vm/hetzner.ts
+++ server/vm/hetzner.ts
@@ -38,20 +38,19 @@ export class Hetzner extends VMManager {
         name: name,
         server_type: this.isLarge ? this.largeSize : this.size,
         start_after_create: true,
-        // TODO temp fix
-        image: this.isLarge ? 'docker-ce' : imageId,
+        image: imageId,
         ssh_keys: sshKeys,
         // networks: [
         //   this.networks[Math.floor(Math.random() * this.networks.length)],
         // ],
-        user_data: this.isLarge
-          ? `#!/bin/bash
-apt-get install -y dnsutils
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
-PASSWORD=$(hostname)
-RESOLUTION=$(if [ "$(nproc)" -le "2" ]; then echo "1280x720@30"; else echo "1920x1080@30"; fi)
-docker run -d --rm --name=vbrowser --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN=$RESOLUTION -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_H264="1" howardc93/vbrowser`
-          : '',
+        //         user_data: this.isLarge
+        //           ? `#!/bin/bash
+        // apt-get install -y dnsutils
+        // iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
+        // PASSWORD=$(hostname)
+        // RESOLUTION=$(if [ "$(nproc)" -le "2" ]; then echo "1280x720@30"; else echo "1920x1080@30"; fi)
+        // docker run -d --rm --name=vbrowser --log-opt max-size=1g --net=host --shm-size=1g --cap-add="SYS_ADMIN" -e DISPLAY=":99.0" -e NEKO_SCREEN=$RESOLUTION -e NEKO_PASSWORD=$PASSWORD -e NEKO_PASSWORD_ADMIN=$PASSWORD -e NEKO_BIND=":5000" -e NEKO_EPR=":59000-59100" -e NEKO_H264="1" howardc93/vbrowser`
+        //           : '',
         labels: {
           [this.getTag()]: '1',
           originalName: name,
@@ -74,11 +73,6 @@ docker run -d --rm --name=vbrowser --log-opt max-size=1g --net=host --shm-size=1
   };
 
   rebootVM = async (id: string) => {
-    // TODO temp fix: create a new VM for large users since rebuilds aren't working right now
-    if (this.isLarge) {
-      this.terminateVM(id);
-      return;
-    }
     // Hetzner does not update the hostname automatically on instance name update + reboot
     // It requires a rebuild command
     // Generate a new password
