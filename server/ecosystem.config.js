d555fb7d354849aa8efbe5af08fef994309f3eb7
Ubuntu <howardzchung@gmail.com>
2023-05-13 17:12:06 +0000

    update vbrowser image
    Former-commit-id: 6e34d13f073e818241433fba5c5e7775df60e2af

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -42,7 +42,7 @@ module.exports = {
       env: {
         HETZNER_GATEWAY: 'gateway2.watchparty.me',
         HETZNER_SSH_KEYS: '1570536',
-        HETZNER_IMAGE: '99649800',
+        HETZNER_IMAGE: '110752622',
         SCW_GATEWAY: 'gateway1.watchparty.me',
         SCW_IMAGE: '',
         DO_GATEWAY: 'gateway4.watchparty.me',
