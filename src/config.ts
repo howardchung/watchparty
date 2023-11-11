71fb6d223100e3591cb32b115907980ad3062831
Ubuntu <howardzchung@gmail.com>
2023-11-11 16:39:22 +0000

    fix recaptcha key
    Former-commit-id: 80f90bd2604d954906b60a89dcaf00d44a0d5afc

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@ export default {
   VITE_OAUTH_REDIRECT_HOSTNAME: import.meta.env.VITE_OAUTH_REDIRECT_HOSTNAME,
   VITE_FIREBASE_CONFIG: import.meta.env.VITE_FIREBASE_CONFIG,
   VITE_STRIPE_PUBLIC_KEY: import.meta.env.VITE_STRIPE_PUBLIC_KEY,
+  VITE_RECAPTCHA_SITE_KEY: import.meta.env.VITE_RECAPTCHA_SITE_KEY,
   NODE_ENV: import.meta.env.DEV ? 'development' : 'production',
 };
