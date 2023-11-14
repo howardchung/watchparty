a950d5ba0ced80f1ce97de51c25c9418c62921fe
Ubuntu <howardzchung@gmail.com>
2023-11-14 18:30:40 +0000

    tsignore

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -472,9 +472,8 @@ export default class App extends React.Component<AppProps, AppState> {
             // magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent
             // Can't import webtorrent directly due to vite build error
             //@ts-ignore
-            const WebTorrent = (
-              await import('webtorrent/dist/webtorrent.min.js')
-            ).default;
+            const WebTorrent = //@ts-ignore
+            (await import('webtorrent/dist/webtorrent.min.js')).default;
             //@ts-ignore
             window.watchparty.webtorrent?._server?.close();
             window.watchparty.webtorrent?.destroy();
