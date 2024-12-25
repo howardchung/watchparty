0189b0623a009508ef30428718fd0ad14ad8e044
Ubuntu <howardzchung@gmail.com>
2024-12-25 07:15:16 +0000

    fix generatename

--- src/utils/generateName.ts
+++ src/utils/generateName.ts
@@ -2,5 +2,5 @@ import { serverPath } from './index';
 
 export async function generateName(): Promise<string> {
   const response = await fetch(serverPath + '/generateName');
-  return response.json();
+  return response.text();
 }
