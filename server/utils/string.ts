3203b7e145fe748c7bcf25e20db302c6e5d62fb6
Ubuntu <howardzchung@gmail.com>
2024-12-26 23:06:12 +0000

    remove batched release, replace hash with cyrb53
    Former-commit-id: 1ae3e0a2a8a351fe6fcd88b6f6333f0af5da0a87

--- server/utils/string.ts
+++
@@ -1,8 +0,0 @@
-export function hashString(input: string) {
-  var hash = 0;
-  for (var i = 0; i < input.length; i++) {
-    var charCode = input.charCodeAt(i);
-    hash += charCode;
-  }
-  return hash;
-}
